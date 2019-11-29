019c085f07bcc8e00804266dc77e3e5e51588312
Aman Ibrahim <amanmibra@gmail.com>
2019-11-29 18:37:43 -0500

    Update main.rb for testing

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 require "date"
 require "faker"
+require "pp"
+require "gthc"
 
 require "./Person"
 require "./Slot"
@@ -21,57 +23,98 @@ end
 
 # testing
 people = Array.new
-for i in 0..10
-  p = Person.new(i, Faker::Name.name, 0, 0, 0, 0)
-  people.push p
-end
-
 slotGrid = Array.new
+currentTime = Time.now
 
-for i in 0..5
-  startDate = Time.now + rand(0..60*60*24) # any time within the next hr
-  endDate = startDate + 60*10 # adding 60 min for 1 hr long Slots
-  phase = ["Black", "Blue", "White"].sample
-  isNight = startDate.hour < 7 && endDate.hour < 7
-  row = i
-  col = 0
+for i in 0..10
+  # create and add person
+  p = Person.new(i, Faker::Name.name, 0, 0, 0, 0)
 
-  people.each do |p|
-    id = p.id
-    status = ["Available", "Unavailable"].sample
+  # create and add their slots
+  startTime = currentTime
+  slots = Array.new
+  for j in 0..5
+    endTime = startTime + 60*60 # make slots 1 hr long
+    isNight = startTime.hour < 7 || endTime.hour < 7
+    row = j
+    col = 0
+
+    status = ["Available", "Unavailable", "Scheduled"].sample
     if status == "Available"
       if isNight
         p.nightFree += 1
       else
         p.dayFree += 1
       end
+    elsif status == "Scheduled"
+      if isNight
+        p.nightScheduled += 1
+      else
+        p.dayScheduled += 1
+      end
     end
+
     slot = Slot.new(
-      id,
-      startDate,
-      endDate,
-      phase,
+      i,
+      startTime,
+      endTime,
+      "Black",
       isNight,
       status,
       row, col
     )
 
-    if !slotGrid[id]
-      slotGrid[id] = Array.new
-    end
-    slotGrid[id].push(slot)
+    slots.push slot
   end
+
+  people.push p
+  slotGrid.push slots
 end
 
-puts "people"
-people.each do |p|
-  puts p.name + " " + p.dayFree.to_s + ", " + p.nightFree.to_s
-end
 
-people, b = Olson.driver people, slotGrid
+# for i in 0..5
+#   startDate = Time.now + rand(0..60*60*24) # any time within the next hr
+#   endDate = startDate + 60*10 # adding 60 min for 1 hr long Slots
+#   phase = ["Black", "Blue", "White"].sample
+#   isNight = startDate.hour < 7 && endDate.hour < 7
+#   row = i
+#   col = 0
+
+#   people.each do |p|
+#     id = p.id
+#     status = ["Available", "Unavailable"].sample
+#     if status == "Available"
+#       if isNight
+#         p.nightFree += 1
+#       else
+#         p.dayFree += 1
+#       end
+#     end
+#     slot = Slot.new(
+#       id,
+#       startDate,
+#       endDate,
+#       phase,
+#       isNight,
+#       status,
+#       row, col
+#     )
+
+#     if !slotGrid[id]
+#       slotGrid[id] = Array.new
+#     end
+#     slotGrid[id].push(slot)
+#   end
+# end
+
+puts "old people"
+pp people[0]
+puts ''
+pp slotGrid[0]
+people, b = GTHC::Olson.driver people, slotGrid
 
 puts ""
 puts "new people"
-people.each do |p|
-  puts p.name + " " + p.dayFree.to_s + ", " + p.nightFree.to_s
-end
+pp people[0]
+puts ''
+pp b[0]
